c85b92af41d5ccdae9d9f62e7172298c94a345d1
capstone_2025_1 <capstone_2025_1@coccoc.com>
2025-08-28 20:56:49 +0700

    Fix: Fix missing retirement check for borrow_book procedure

--- db/borrow_book.sql
+++ db/borrow_book.sql
@@ -3,6 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE borrow_book(IN p_user_id INT, IN p_book_id INT)
 BEGIN
   DECLARE v_copies_left INT;
+  DECLARE v_is_retired BOOLEAN;
 
   DECLARE EXIT HANDLER FOR SQLEXCEPTION
   BEGIN
@@ -18,14 +19,24 @@ BEGIN
    WHERE book_id = p_book_id
      FOR UPDATE;
 
-  IF is_book_available(p_book_id) = 0 THEN
+  SELECT EXISTS(
+      SELECT 1
+        FROM book_retirement
+       WHERE book_id = p_book_id
+  )
+  INTO v_is_retired;
+
+  IF v_is_retired = TRUE THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Book is retired and cannot be borrowed';
+  END IF;
+
+  IF v_copies_left <= 0 THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No available copies left';
   END IF;
 
   INSERT INTO checkout (user_id, book_id, borrow_date, due_date)
   VALUES (p_user_id, p_book_id, CURRENT_TIMESTAMP,
           DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 MONTH));
-
   COMMIT;
 END $$
 DELIMITER ;
