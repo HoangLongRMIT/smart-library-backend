36d84ba196712de7dd424f150e9906fbd6e8b8d8
ProdDG <s3911444@rmit.edu.vn>
2025-09-03 21:42:12 +0700

    removed partitions and fixed indexing code

--- db/init.sql
+++ db/init.sql
@@ -744,6 +744,8 @@ INSERT INTO staffLog (user_id, book_id, action, timestamp) VALUES
 CREATE INDEX idx_book_title ON book(title);
 CREATE INDEX idx_book_genre ON book(genre);
 CREATE INDEX idx_publisher_id ON book(publisher);
+CREATE INDEX idx_author_name ON author(name);
+CREATE INDEX idx_book_author ON book(author_id);
 
 -- Optimization for reports
 
@@ -755,24 +757,3 @@ CREATE INDEX idx_checkout_user_borrow_date ON checkout(user_id, borrow_date);
 
 -- Books with low availability
 CREATE INDEX idx_checkout_book_return_date ON checkout(book_id, return_date);
-
--- Optimization to improve query performance: Partitions
-
--- Partitioning checkout table by month of borrow_date
--- Currently not possible due to conflict with foreign key constraint
-
--- ALTER TABLE checkout
--- PARTITION BY RANGE (TO_DAYS(borrow_date)) (
--- 	PARTITION p2025_01 VALUES LESS THAN (TO_DAYS('2025-02-01')),
---   PARTITION p2025_02 VALUES LESS THAN (TO_DAYS('2025-03-01')),
---   PARTITION p2025_03 VALUES LESS THAN (TO_DAYS('2025-04-01')),
---   PARTITION p2025_04 VALUES LESS THAN (TO_DAYS('2025-05-01')),
---   PARTITION p2025_05 VALUES LESS THAN (TO_DAYS('2025-06-01')),
---   PARTITION p2025_06 VALUES LESS THAN (TO_DAYS('2025-07-01')),
---   PARTITION p2025_07 VALUES LESS THAN (TO_DAYS('2025-08-01')),
---   PARTITION p2025_08 VALUES LESS THAN (TO_DAYS('2025-09-01')),
---   PARTITION p2025_09 VALUES LESS THAN (TO_DAYS('2025-10-01')),
---   PARTITION p2025_10 VALUES LESS THAN (TO_DAYS('2025-11-01')),
---   PARTITION p2025_11 VALUES LESS THAN (TO_DAYS('2025-12-01')),
---   PARTITION p2025_12 VALUES LESS THAN (TO_DAYS('2026-01-01'))
--- );
